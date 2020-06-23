53c8fb5c8dcf6a3257a1bc08b01197f4c9dc4c8b
stevieismagic <stevieismagic@gmail.com>
2020-06-23 11:17:02 -0700

    notes from documentation

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -4,6 +4,7 @@ const typeDefs = gql`
 
   # SCHEMAS
   
+  #the Launch object type has a collection of fields, and each field has a type of its own
   type Launch {
   id: ID!
   site: String
@@ -11,12 +12,14 @@ const typeDefs = gql`
   rocket: Rocket
   isBooked: Boolean!
 
+  # ! - means this field's value can never be null
   type Rocket {
   id: ID!
   name: String
   type: String
   }
 
+  # if an array has an exclamation point after it, the array cannot be null, but it CAN be empty
   type User {
     id: ID!
     email: String!
@@ -33,17 +36,14 @@ const typeDefs = gql`
     LARGE
   }
 
+  # return Type of bookTrips/cancelTrips mutations
   TripUpdateResponse {
     success: Boolean!
     message: String
     launches: [Launch]
   }
 
-
-
-
-
-  # QUERIES
+  # QUERIES - allow client to fetch data
 
   type Query {
     launches: [Launch]!
@@ -51,8 +51,9 @@ const typeDefs = gql`
     me: User
   }
 
-  # MUTATIONS
+  # MUTATIONS - allow client to modify data
 
+  # it's good practice for a mutation to return whatever objects it modifies, so the requesting client can update its cache an UI without need to make a followup request
   type Mutation {
     bookTrips(launchIds: [ID]!): TripUpdateResponse!
     cancelTrip(launchId: ID!): TripUpdateResponse!
