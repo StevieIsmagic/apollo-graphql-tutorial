ff17ae5c1d0e0cd4de96996b0477311833f61458
stevieismagic <stevieismagic@gmail.com>
2020-06-24 12:51:59 -0700

    fetch all launches and transform response data

---
+++ start/server/src/datasources/launch.js
@@ -0,0 +1,55 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+const { getArgumentValues } = require('graphql/execution/values');
+
+/*
+The RESTDataSource class automatically caches responses from REST resources with no additional setup. We call this feature partial query caching. It enables you to take advantage of the caching logic that the REST API already exposes.
+
+Note - we moved from Class components b/c SOLID Principles
+composition vs configurtion
+hi(there(everyone)) vs ...lots of overhead(this..that=this)
+*/
+class LaunchAPI extends RESTDataSource {
+  // what does this do again ?
+  // need look deeper into these
+      constructor() {
+    super();
+    this.baseURL = 'https://api.spacexdata.com/v2/';
+  }
+
+  /*
+  Reducer - transforms returned launch data into the shape that our schema expects. This approach decouples the structure of your schema from the structure of the various data sources that populate its fields.
+
+  Note - see MapReduce (export - transport - load)
+  */
+
+  launchReducer(launch) {
+    return {
+      id: launch.flight_number || 0,
+      cursor: `${launch.launch_date_unix}`,
+      site: launch.launch_site && launch.launch_site.site_name,
+      mission: {
+        name: launch.mission_name,
+        missionPatchSmall: launch.links.mission_patch_small,
+        missionPatchLarge: launch.links.mission_patch,
+      },
+      rocket: {
+        id: launch.rocket.rocket_id,
+        name: launch.rocket.rocket_name,
+        type: launch.rocket.rocket_type,
+      },
+    };
+  }
+
+}
+
+async getAllLaunches() {
+  const response = await this.get('launches');
+  return Array.isArray(response) ? response.map(launch => this.launchReducer(launch)) : [];
+}
+
+
+
+
+
+
+module.exports = LaunchAPI;
