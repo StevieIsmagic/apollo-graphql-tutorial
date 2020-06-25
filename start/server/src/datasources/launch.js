8c9c5c9a66186b4eb4d237715c54c0921d7e39a0
stevieismagic <stevieismagic@gmail.com>
2020-06-25 09:23:46 -0700

    connect SQLite and spaceX API datasources to Apollo Server

--- start/server/src/datasources/launch.js
+++ start/server/src/datasources/launch.js
@@ -40,24 +40,24 @@ class LaunchAPI extends RESTDataSource {
     };
   }
 
-}
-
-async getAllLaunches() {
-  const response = await this.get('launches');
-  return Array.isArray(response) ? response.map(launch => this.launchReducer(launch)) : [];
-}
-
-async getLaunchById({ launchId }) {
-  const response = await this.get('launches', { flight_number: launchId });
-  return this.launchReducer(response[0]);
-}
-
-getLaunchesByIds({ launchIds }) {
-  return Promise.all(
-    launchIds.map(launchId => this.getLaunchById({ launchId })),
-  );
-}
-
+  
+  async getAllLaunches() {
+    const response = await this.get('launches');
+    return Array.isArray(response) ? response.map(launch => this.launchReducer(launch)) : [];
+  }
+  
+  async getLaunchById({ launchId }) {
+    const response = await this.get('launches', { flight_number: launchId });
+    return this.launchReducer(response[0]);
+  }
+  
+  getLaunchesByIds({ launchIds }) {
+    return Promise.all(
+      launchIds.map(launchId => this.getLaunchById({ launchId })),
+      );
+    }
+    
+  }
 
 
 
