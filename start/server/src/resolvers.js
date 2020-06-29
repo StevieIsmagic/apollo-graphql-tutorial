0d4a90f3b9ffec86d5757d93d52daa19eb82a976
stevieismagic <stevieismagic@gmail.com>
2020-06-29 07:02:16 -0700

    notes on function signatire

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -7,3 +7,19 @@ module.exports = {
     me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
   }
 };
+
+
+/*
+RESOLVER FUNCTION SIGNATURE
+
+fieldName: (parent, args, context, info) => data;
+
+  parent - return value of the resolver for this field's parent
+
+  args - object that contains all the GraphQL arguments     provided for this field
+
+  context - object that is shared across all resolvers that execute for a particular operation. Use this to share per-operation-state, such as authentication information and access to data sources.
+  
+  info - contains information about execution state of operation - used only in advanced state
+
+*/ 
