b8df8259e2e0c45949a4986a9ea861e6a33f078b
stevieismagic <stevieismagic@gmail.com>
2020-06-30 07:30:03 -0700

    add missionPatch resolver

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -5,7 +5,33 @@ module.exports = {
     launch: (_, { id }, { dataSources }) =>
       dataSources.launchAPI.getLaunchById({ launchId: id }),
     me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
-  }
+  },
+
+  Mission: {
+    // The default size is 'LARGE' if not provided
+    missionPatch: (mission, { size } = { size: 'LARGE' }) => {
+      return size === 'SMALL' 
+        ? mission.missionPatchSmall
+        : mission.missionPatchLarge;
+    },
+  },
+  // isBooked is a resolver for the isBooked field on the Launch type (schema.js)
+  Launch: {
+    isBooked: async (launch, _, { dataSources}) => dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id }),
+  },
+  User: {
+    trips: async (_, __, { dataSources }) => {
+      // get ids of launches by user
+      const launchIds = await dataSources.userAPI.getLaunchIdsByUser();
+      if (!launchIds.length) return [];
+      // look up those launches by their ids
+      return (
+        dataSources.launchAPI.getLaunchesByIds({ launchIds }) || []
+      );
+    },
+  },
+
+
 };
 
 
