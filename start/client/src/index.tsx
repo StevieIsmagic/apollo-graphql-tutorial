ee9163f3796b924d4a83829b4f3cbaa175dcd4b6
stevieismagic <stevieismagic@gmail.com>
2020-07-28 07:08:50 -0700

    finished

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,10 +1,12 @@
 import { ApolloClient } from 'apollo-client';
+import gql from 'graphql-tag';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
 import React from 'react';
 import ReactDOM from 'react-dom'; 
 import Pages from './pages';
+import Login from './pages/login';
 import injectStyles from './styles';
 import { resolvers, typeDefs } from './resolvers';
 
@@ -30,10 +32,21 @@ cache.writeData({
   },
 });
 
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
 injectStyles();
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
   </ApolloProvider>, 
   document.getElementById('root')
 );
